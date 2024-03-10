2a65e70b4a27cc9470ba484d8a80f41617980ffd
Shaganov Vyacheslav <slavikshag@gmail.com>
2024-03-10 12:24:20 +0300

    wind force added

--- missile_motion/system/MissileSystem.cpp
+++ missile_motion/system/MissileSystem.cpp
@@ -30,21 +30,12 @@ void MissileSystem::f(Vector &state, double time) const
     double x = state[3], y = state[4], z = state[5];
     double omega_x = state[6], p = state[7];
 
-    // Vector r = {x,y,z};
-    // auto rMinusProj = r - LinAlg::projectionOnEllipse(
-    //     r, 
-    //     Constants::Earth::MAJOR_AXIS, 
-    //     Constants::Earth::MAJOR_AXIS, 
-    //     Constants::Earth::MINOR_AXIS);
-    // // if fell
+    // if fell
     if (y < 0) {
         state = Vector(8); // {0, 0, 0, 0, 0, 0, 0, 0};
         return;
     }
 
-    // double height_sqr = rMinusProj.dot(rMinusProj);
-    // double height = sqrt(height_sqr);
-    // std::cout << "h: " << height << '\n';
     auto atm = (*atmosphere)(y);
     
     double g = 9.81;
@@ -58,12 +49,36 @@ void MissileSystem::f(Vector &state, double time) const
     double h = params->missile.distanceBaseCM + 0.57 * params->missile.length - 0.16 * params->missile.diameter;
     
     double f_z = L*L / (params->missile.diameter * h) * (*Knm)(M);
-    double q = (*atmosphere)(0).density * pow((*atmosphere)(0).soundSpeed, 2) * M*M * p / 2; // mb incorrect (p)
+    double q = (*atmosphere)(0).density * pow((*atmosphere)(0).soundSpeed, 2) * M*M * p / 2;
     double S_m = M_PI * pow(params->missile.diameter / 2, 2);
+    
+    // wind
+    double w = params->environment.windVelocity;
+    double a_w = params->environment.windAzimuth;
+    double gamma_w = params->environment.windElevationAngle;
+    
+    double w_x = w * cos(a_w) * sin(gamma_w);
+    double w_y = w * sin(gamma_w);
+    double w_z = w * cos(a_w) * cos(gamma_w);
+
+    double w_x_k = w_x * cos(theta) * cos(psi) + w_y * sin(theta) - w_z * cos(theta) * sin(psi);
+    double w_y_k = -w_x * sin(theta) * cos(psi) + w_y * sin(theta) - w_z * cos(theta) * sin(psi);
+    double w_z_k = w_x * sin(psi) + w_z * cos(psi);
+
+    double V_x = v_k - w_x_k, V_y = -w_y_k, V_z = -w_z_k;
+    double V = sqrt(V_x*V_x + V_y*V_y + V_z*V_z);
+
+    double eps_w2 = asin(-w_y_k/V);
+    double eps_w1 = asin(w_z_k/(V*cos(eps_w2)));
+    // a.r. parts of de
+    double qSmixCx = q * S_m * i_x * (*Cx)(M);
+    double v_k_d_ar = -qSmixCx * cos(eps_w1) * sin(eps_w2) / m;
+    double theta_d_ar = -qSmixCx * sin(eps_w2) / (m * v_k);
+    double psi_d_ar = -qSmixCx * sin(eps_w1) * cos(eps_w2) / (m * v_k * cos(theta));
     // right side
-    double v_k_d = -g * sin(theta);
-    double theta_d = -g * cos(theta) / v_k;
-    double psi_d = -i_z * f_z * J_x * omega_x * theta_d / (L * m * v_k * cos(theta));
+    double v_k_d = -g * sin(theta) + v_k_d_ar;
+    double theta_d = -g * cos(theta) / v_k + theta_d_ar;
+    double psi_d = -i_z * f_z * J_x * omega_x * theta_d / (L * m * v_k * cos(theta)) + psi_d_ar;
 
     double x_d = v_k * cos(theta) * cos(psi);
     double y_d = v_k * sin(theta);
